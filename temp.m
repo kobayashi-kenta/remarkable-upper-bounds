5dde1815e5c748ce95088530ba58c1d02136b76e
kobayashi-kenta <kenta.k@r.hit-u.ac.jp>
2025-07-05 00:22:34 +0900

    Add files via upload

--- temp.m
+++ temp.m
@@ -1,5 +1,4 @@
 function L(a, b)
     BB = a^2 + b^2;
-    z = AA*BB*CC / 16 / SS - (AA + BB + CC) / 30 ...
-        - SS / 5 * (1/AA + 1/BB + 1/CC);
+    z = AA*BB*CC / 16 / SS - (AA + BB + CC) / 30;
 end
