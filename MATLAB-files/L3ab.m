8ad60d2eb883bdf56b93217ac6d06f5a35598350
kobayashi-kenta <kenta.k@r.hit-u.ac.jp>
2026-07-05 09:55:53 +0900

    Delete MATLAB-files directory

--- MATLAB-files/L3ab.m
+++
@@ -1,8 +0,0 @@
-function z = L3ab(a, b)
-    AA = (1-a)^2 + b^2;
-    BB = a^2 + b^2;
-    CC = 1;
-    SS = b^2 / 4;
-    z = (AA*BB + BB*CC + CC*AA) / 83 ...
-        - (AA*BB*CC / (AA + BB + CC) + SS) / 24;
-end
